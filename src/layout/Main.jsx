a97b6894d6e6c87a7d9b1ff9087a6cd95f3a650a
Larchenko <larchenko@lanit.ru>
2022-12-03 00:04:58 +0300

    catch error

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -13,15 +13,23 @@ export class Main extends React.Component {
 
   searchMovies = (str, type = "all") => {
     this.setState({ loading: true });
-    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
+    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
       .then(response => response.json())
-      .then(data => this.setState({ movies: data.Search, loading: false }));
+      .then(data => this.setState({ movies: data.Search, loading: false }))
+      .catch((err) => {
+        console.log(err);
+        this.setState({ loading: false });
+      });
   }
 
   componentDidMount() {
-    fetch('http://www.omdbapi.com/?apikey=ce38fcfc&s=hello')
+    fetch('https://www.omdbapi.com/?apikey=ce38fcfc&s=hello')
       .then(response => response.json())
-      .then(data => this.setState({ movies: data.Search, loading: false }));
+      .then(data => this.setState({ movies: data.Search, loading: false }))
+      .catch((err) => {
+        console.log(err);
+        this.setState({ loading: false });
+      });
   }
 
   render() {
